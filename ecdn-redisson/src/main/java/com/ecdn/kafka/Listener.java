51ad0930b6dcc9a44ef46f9b9c485b19dc22a392
DESKTOP-G9BR8QQ\Park <park@oneplat.co>
2017-09-15 11:15:47 +0900

    소스정리

--- ecdn-redisson/src/main/java/com/ecdn/kafka/Listener.java
+++ ecdn-redisson/src/main/java/com/ecdn/kafka/Listener.java
@@ -9,7 +9,7 @@ public class Listener {
 	
 	public final CountDownLatch countDownLatch1 = new CountDownLatch(1);
 
-	@KafkaListener(id = "foo", topics = "topic1", group = "group1")
+	@KafkaListener(id = "foo", topics = "topic1")
 	public void listen(ConsumerRecord<?, ?> record) {
 		System.out.println("컨슈머 리스너 테스트 중 : "+record.value());
 		countDownLatch1.countDown();
